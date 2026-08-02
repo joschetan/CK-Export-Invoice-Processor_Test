94ea0ea8650b4840c223b3759f084bbbdf03d622
joschetan <joschetan@gmail.com>
2026-08-02 13:43:20 +0530

    Create parser_bkt.py

---
+++ parser_bkt.py
@@ -0,0 +1,13 @@
+import re
+
+def extract_bkt_items(pdf_lines):
+    """
+    BKT / Tyre Format Dedicated Item Table Parser Logic (Placeholder).
+    Will be configured precisely when you are ready to write rules for BKT format.
+    """
+    parsed_items = []
+    
+    # BKT format usually has Material Grp, HS Code, Quantity, Value, Gross/Net Weight etc.
+    # We will implement this parser precisely as per BKT's PDF layout when requested.
+    
+    return parsed_items
